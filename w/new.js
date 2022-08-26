9560a79a7aaea8833ba2af1a5452efd2438bfd59
Paulcode2 <57816204+Paulcode2@users.noreply.github.com>
2022-08-26 23:34:56 +0100

    Add files via upload

---
+++ w/new.js
@@ -0,0 +1,13 @@
+function Submitted(){
+    document.write('Thank you for your response, we will get back to you.');
+}
+
+
+// function Submitted(){
+//     let response;
+//     if (response === 'Yes'){
+//     alert('Welcome to the space.');
+// } else if (response === 'No'){
+//     alert('Thank you for your time, we hope you change your mind.')
+// }
+// }
